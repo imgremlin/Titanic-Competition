8d2a8f04bb00050a3ec5dd901aef6852f8d5cc8b
Alex <40468037+imgremlin@users.noreply.github.com>
2020-04-04 18:45:49 +0300

    0.82775

--- titanic_main.py
+++ titanic_main.py
@@ -3,15 +3,24 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
-import category_encoders as ce
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 train = pd.read_csv('train.csv', index_col='PassengerId')
 test = pd.read_csv('test.csv', index_col='PassengerId')
 
-#CONCAT
+#concatenating train&test
 X = pd.concat([train.drop("Survived", axis=1), test])
 y=train['Survived']
 
+#extracting title from Name
+X['Title']=X.Name.str.extract(",\s(\w+).")
+
+X['Is_Married'] = 0
+X.loc[X.Title == 'Mrs', 'Is_Married'] = 1
+
+#making less features through mapping
 titles_dict = {'Lady':'Miss/Mrs/Ms', 'the':'Miss/Mrs/Ms','Capt':'Rare', 'Col':'Rare',
                 'Don':'Rare', 'Dr':'Rare','Major':'Rare', 'Rev':'Rare',
                 'Sir':'Rare', 'Jonkheer':'Rare', 'Dona':'Miss/Mrs/Ms',
@@ -19,14 +28,9 @@ titles_dict = {'Lady':'Miss/Mrs/Ms', 'the':'Miss/Mrs/Ms','Capt':'Rare', 'Col':'R
                 'Mr':'Mr','Mrs':'Miss/Mrs/Ms',
                 'Miss':'Miss/Mrs/Ms','Master':'Master'}
 
-X['Title']=X.Name.str.extract(",\s(\w+).")
-
-X['Is_Married'] = 0
-
-X.Is_Married[X['Title'] == 'Mrs'] = 1
-
 X['Title'] = X['Title'].map(titles_dict)
 
+#extract surname from Name
 X['Family'] = X.Name.str.extract("([A-Za-z ]+),")
 
 X['Sex']=X['Sex'].map({'male':1, 'female':0})
@@ -36,27 +40,32 @@ X['Family_Size'] = X['SibSp'] + X['Parch'] + 1
 family_map = {1: 'Alone', 2: 'Small', 3: 'Small', 4: 'Small', 5: 'Medium', 6: 'Medium', 7: 'Large', 8: 'Large', 11: 'Large'}
 X['Family_Grouped'] = X['Family_Size'].map(family_map)
 
-#median, not mean
+#dealing with missing data via fill in median age of group by Sex&Pclass
 X['Age'] = X.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.mean()))
 
-#adding new feature
 X['Ticket_Frequency'] = X.groupby('Ticket')['Ticket'].transform('count')
 
+#fill in the most popular feature
 X['Embarked']=X['Embarked'].fillna('S')
 
+#deal with missing data via fill in median age of group by Parch&SibSp&Pclass
 na_fare = X.groupby(['Pclass', 'Parch', 'SibSp']).Fare.median()[3][0][0]
 X['Fare'] = X['Fare'].fillna(na_fare)
 
+#transform continious features
 X['Age'] = pd.qcut(X['Age'], 10)
-
 X['Fare'] = pd.qcut(X['Fare'], 13)
 
+#making Pclass string to one hot encode it 
 X['Pclass']=X['Pclass'].apply(str)
 
+#filling Cabin missing data
 X['Deck'] = X['Cabin'].apply(lambda s: s[0] if pd.notnull(s) else 'M')
 
-X.Deck[X['Deck'] == 'T'] = 'A'
+#'T' and 'A' have got almost the same distribution of passengers
+X.loc[X['Deck'] == 'T', 'Deck'] = 'A'
 
+#replacing to make less features
 X['Deck'] = X['Deck'].replace(['A', 'B', 'C'], 'ABC')
 X['Deck'] = X['Deck'].replace(['D', 'E'], 'DE')
 X['Deck'] = X['Deck'].replace(['F', 'G'], 'FG')
@@ -66,11 +75,8 @@ X.drop(['Cabin'], inplace=True, axis=1)
 train = X.loc[train.index]
 train =train.join(y)
 test = X.loc[test.index]
-#CONCAT
 
-
-#BIG FUNCTION @@@@@@@@@@
-
+#function to calculate survival rate with help of Family&Tickets
 non_unique_families = [x for x in train['Family'].unique() if x in test['Family'].unique()]
 non_unique_tickets = [x for x in train['Ticket'].unique() if x in test['Ticket'].unique()]
 
@@ -146,8 +152,8 @@ for df in [train, test]:
     df['Survival_Rate'] = 0.5*(df['Ticket_Survival_Rate'] + df['Family_Survival_Rate'])
     df['Survival_Rate_NA'] = 0.5*(df['Ticket_Survival_Rate_NA'] + df['Family_Survival_Rate_NA'])   
 
-#BIG FUNCTION ENDS @@@@@@@@@@
 
+#using Label Encoder to transform categorical features to numerical
 non_numeric_features = ['Embarked', 'Deck', 'Title', 'Family_Grouped', 'Age', 'Fare']
 
 for df in [train, test]:
@@ -156,6 +162,7 @@ for df in [train, test]:
 
 cat_features = ['Embarked', 'Deck', 'Title', 'Family_Grouped','Pclass', 'Sex']
 
+#creating one hot encoded features
 encoded_features = []
 
 for df in [train, test]:
@@ -167,76 +174,31 @@ for df in [train, test]:
         encoded_df.index = df.index
         encoded_features.append(encoded_df)
 
+#adding encoded features
 train = pd.concat([train, *encoded_features[:6]], axis=1)
 test = pd.concat([test, *encoded_features[6:]], axis=1)
 
-drop_cols = ['Deck', 'Embarked', 'Family', 'Family_Size', 'Family_Grouped',
-             'Name', 'Parch', 'Pclass', 'Sex', 'SibSp', 'Ticket', 'Title',
+
+#dropping unnecessary features
+drop_cols = ['Deck', 'Embarked', 'Family',  'Family_Grouped',
+             'Name',  'Pclass',  'Ticket', 'Title',
             'Ticket_Survival_Rate', 'Family_Survival_Rate',
-            'Ticket_Survival_Rate_NA', 'Family_Survival_Rate_NA']
+            'Ticket_Survival_Rate_NA', 'Family_Survival_Rate_NA',
+            'Sex', 'SibSp','Parch','Family_Size']
 
 train.drop(columns=drop_cols, inplace=True)
 test.drop(columns=drop_cols, inplace=True)
 
-def fam():
+
+def count_survival_plot(column):
     f, axes = plt.subplots(2, figsize=(10, 10))
-    fam_size=train.groupby('Fam')['Survived'].mean().sort_values(ascending=True)
+    fam_size=train.groupby(column)['Survived'].mean().sort_values(ascending=True)
     sns.barplot(x=fam_size.keys(), y=fam_size.values,
                 ax=axes[0]).set_title('Death ratio')
-    fam_count_plot_series=train.groupby('Fam')['Pclass'].count().sort_values(ascending=True)
+    fam_count_plot_series=train.groupby(column)['Pclass'].count().sort_values(ascending=True)
     sns.barplot(x=fam_count_plot_series.keys(), y=fam_count_plot_series.values,
                 ax=axes[1]).set_title('Number of each')
 
-def fare():
-    f, axes = plt.subplots(2, figsize=(10, 10))
-    fare_ratio=train.groupby('FareBins')['Survived'].mean().sort_values(ascending=True)
-    sns.barplot(x=fare_ratio.keys(), y=fare_ratio.values,
-                ax=axes[0]).set_title('Death ratio')
-    fare_count=train.groupby('FareBins')['Pclass'].count().sort_values(ascending=True)
-    sns.barplot(x=fare_count.keys(), y=fare_count.values,
-                ax=axes[1]).set_title('Number of each')
-    
-def age():
-    f, axes = plt.subplots(2, figsize=(10, 10))
-    fam_size=train.groupby('AgeBins')['Survived'].mean().sort_values(ascending=True)
-    sns.barplot(x=fam_size.keys(), y=fam_size.values,
-                ax=axes[0]).set_title('Death ratio')
-    fam_count_plot_series=train.groupby('AgeBins')['Pclass'].count().sort_values(ascending=True)
-    sns.barplot(x=fam_count_plot_series.keys(), y=fam_count_plot_series.values,
-                ax=axes[1]).set_title('Number of each')
-
-def pclass():
-    pclass_death_ration=train.groupby('Pclass')['Survived'].mean().sort_values(ascending=True)
-    plt.figure(figsize=(10,7))
-    sns.barplot(x=pclass_death_ration.keys(), y=pclass_death_ration.values)
-    plt.show()
-
-def count_names(df):
-    f, axes = plt.subplots(2, figsize=(10, 10))
-    fare_ratio=train.groupby('Title')['Survived'].mean().sort_values(ascending=True)
-    sns.barplot(x=fare_ratio.keys(), y=fare_ratio.values,
-                ax=axes[0]).set_title('Death ratio')
-    fare_count=train.groupby('Title')['Pclass'].count().sort_values(ascending=True)
-    sns.barplot(x=fare_count.keys(), y=fare_count.values,
-                ax=axes[1]).set_title('Number of each')      
-    
-def cabin():
-    f, axes = plt.subplots(2, figsize=(10, 10))
-    fam_size=train.groupby('Cabin')['Survived'].mean().sort_values(ascending=True)
-    sns.barplot(x=fam_size.keys(), y=fam_size.values,
-                ax=axes[0]).set_title('Death ratio')
-    fam_count_plot_series=train.groupby('Cabin')['Pclass'].count().sort_values(ascending=True)
-    sns.barplot(x=fam_count_plot_series.keys(), y=fam_count_plot_series.values,
-                ax=axes[1]).set_title('Number of each')
-
-def embarked():
-    f, axes = plt.subplots(2, figsize=(10, 10))
-    fam_size=train.groupby('Embarked')['Survived'].mean().sort_values(ascending=True)
-    sns.barplot(x=fam_size.keys(), y=fam_size.values,
-                ax=axes[0]).set_title('Death ratio')
-    fam_count_plot_series=train.groupby('Embarked')['Pclass'].count().sort_values(ascending=True)
-    sns.barplot(x=fam_count_plot_series.keys(), y=fam_count_plot_series.values,
-                ax=axes[1]).set_title('Number of each')
 
 def corr_matrix(matrix):
     corr = matrix.corr()
@@ -247,26 +209,28 @@ def corr_matrix(matrix):
                 cmap=sns.diverging_palette(0, 200, n=200)) 
     plt.title('Correlation Heatmap of Numeric Features')
 
+#creating model
 from sklearn.ensemble import RandomForestClassifier
 
 rf = RandomForestClassifier(n_estimators=1750, max_depth=7, 
                             min_samples_split=6, min_samples_leaf=6,
-                            max_features='auto', random_state=42 ) 
+                            max_features='auto', random_state=42) 
 
-
+#scaling our df`s
 X_train = StandardScaler().fit_transform(train.drop(columns=['Survived']))
 y_train = train['Survived'].values
 X_test = StandardScaler().fit_transform(test)
 
-rf.fit(X_train,y_train)
+def feature_importance_plot():
+    rf.fit(X_train,y_train)
+    feat_series = pd.Series(rf.feature_importances_, index =train.drop(columns=['Survived']).columns).sort_values(ascending=False)
+    plt.figure(figsize=(10,7))
+    sns.barplot(y=feat_series.keys(), x=feat_series.values)
 
-
-feat_series = pd.Series(rf.feature_importances_, index =train.drop(columns=['Survived']).columns).sort_values(ascending=False)
-plt.figure(figsize=(10,7))
-sns.barplot(y=feat_series.keys(), x=feat_series.values)
-
+#number of folds 
 split_num = 4
 
+#making dataframe to check cross validation results
 probs = pd.DataFrame(np.zeros((len(X_test), split_num )), columns=['fold_{}'.format(i) for i in range(1, split_num + 1)])
 
 from sklearn.model_selection import KFold
@@ -278,15 +242,15 @@ for fold, (trn_idx, val_idx) in enumerate(kfold.split(X_train, y_train), 1):
     rf.fit(X_train[trn_idx], y_train[trn_idx])
     probs.loc[:, 'fold_{}'.format(fold)] = rf.predict_proba(X_test)[:, 1] 
 
-
+#calculating accuracy of our model
 from sklearn.model_selection import cross_val_score
 
 results = cross_val_score(rf, X_train, y_train, cv=kfold, scoring='accuracy')
 print(f"res kfold: {results.mean():.5f}")
 
+#final result
 probs['res'] = round(probs.mean(axis=1)).astype(int)
 
-#output = pd.DataFrame({'PassengerId': test.index,'Survived': probs['res']})
-
-#output.to_csv('submission.csv', index=False)
-#print(output.head(20))
+#making submission
+output = pd.DataFrame({'PassengerId': test.index,'Survived': probs['res']})
+output.to_csv('submission.csv', index=False)
